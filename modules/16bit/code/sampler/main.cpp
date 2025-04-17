d313204847bacaa1ff3a5459e241c55dd56fe95f
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-04-17 17:49:48 +0530

    Playing a kick sample

--- modules/16bit/code/sampler/main.cpp
+++ modules/16bit/code/sampler/main.cpp
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "io.h"
+#include "kick.h"
+#include "DAC.h"
+
+#define SAMPLE_RATE 44100
+#define BCK_PIN 0
+
+int16_t* KICK_SAMPLES = (int16_t*)kick_wav;
+uint32_t KICK_SAMPLES_LEN = kick_wav_len / 2;
 
 IO *io = IO::getInstance();
+DAC dac(pio0, 0, BCK_PIN);
 
 int main() {
     stdio_init_all();
 
     io->init();
+    dac.init(SAMPLE_RATE);
+
+    uint16_t sampleIndex = 0;
 
     while (true) {
-        io->setLED(true);
-        sleep_ms(250);
-        io->setLED(false);
-        sleep_ms(250);
+        io->update();
+        dac.writeMono(KICK_SAMPLES[sampleIndex], KICK_SAMPLES[sampleIndex]);
+        sampleIndex++;
+        if (sampleIndex >= KICK_SAMPLES_LEN) {
+            sampleIndex = 0;
+            sleep_ms(50);
+        }
     }
 
     return 0;
