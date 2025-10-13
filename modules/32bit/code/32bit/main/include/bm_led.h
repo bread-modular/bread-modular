c5aa16fb9612fb2dad83ffbbe52f3f5f678055c9
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-10-13 22:51:17 +0530

    Add the led support

---
+++ modules/32bit/code/32bit/main/include/bm_led.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bm_setup_led(void);
+void bm_set_led_state(bool on);
--- modules/32bit/code/32bit/main/main.c
+++ modules/32bit/code/32bit/main/main.c
@@ -1,10 +1,17 @@
 #include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+#include "bm_led.h"
 
 void app_main(void)
 {
+    bm_setup_led();
+
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        bm_set_led_state(true);
+        vTaskDelay(pdMS_TO_TICKS(250));
+
+        bm_set_led_state(false);
+        vTaskDelay(pdMS_TO_TICKS(250));
     }
 }
