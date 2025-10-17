966a7c55e12c3f3faf645c423717d68d0e284b86
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-10-17 21:56:52 +0530

    Add proper delay example with circular buffer.

--- modules/32bit/code/32bit/main/main.c
+++ modules/32bit/code/32bit/main/main.c
@@ -13,9 +13,20 @@
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
+#include <sys/cdefs.h>
+
+#define SAMPLE_BUFFER_LEN 44100 // 1 sec
 
 float feedback = 0.0;
-int16_t prev_sample = 0; 
+int16_t sample_buffer[SAMPLE_BUFFER_LEN];
+size_t next_buffer_index = 0;
+
+// even though, we don't go minus range, we do some smoothing 
+// so, (target_delay_samples - delay_samples) can go negative.
+// that's why we need signed ints otherwise, -1 becomes the largest int value
+// then array will go out of index.
+int target_delay_samples = 1;
+int delay_samples = 1;
 
 static void on_button_press() {
     ESP_LOGI("button_press", "button_changed");
@@ -29,14 +40,24 @@ static void on_button_press() {
 inline static void audio_loop(size_t n_samples, int16_t* input, int16_t* output) {
     for (int lc=0; lc<n_samples; lc += 2) {
         int16_t curr = input[lc];
-        int32_t next = ((1 - feedback) * curr) + (feedback * prev_sample);
+
+        delay_samples += (target_delay_samples - delay_samples) * 0.001f;
+
+        int32_t prev_buffer_index = next_buffer_index - delay_samples;
+        prev_buffer_index = prev_buffer_index < 0 ? SAMPLE_BUFFER_LEN + prev_buffer_index: prev_buffer_index;
+        int16_t prev = sample_buffer[prev_buffer_index];
+        
+        int32_t next = ((1 - feedback) * curr) + (feedback * prev);
 
         // clamping
         next = next >  INT16_MAX? INT16_MAX : next;
         next = next < INT16_MIN? INT16_MIN : next;
 
         output[lc] = (int16_t)next;
-        prev_sample = output[lc];
+
+        // buffering
+        sample_buffer[next_buffer_index] = (int16_t)next;
+        next_buffer_index = (next_buffer_index + 1) % SAMPLE_BUFFER_LEN;
     }
 }
 
@@ -60,6 +81,9 @@ void app_main(void)
 
     while (1) {
         feedback = fmin(bm_get_cv1_norm(), 0.99f);
+        target_delay_samples = (bm_get_cv2_norm() * (SAMPLE_BUFFER_LEN - 1));
+        target_delay_samples = target_delay_samples == 0? 1 : target_delay_samples;
+        ESP_LOGI("main", "feedback:%f, delay_samples:%d", feedback, delay_samples);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
