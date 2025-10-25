38f7555e900baddd06a1531858ee3b22a6fdb59f
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-10-25 10:54:05 +0530

    Introduce the struct

--- modules/32bit/code/32bit/main/src/apps/bm_app_reverb.c
+++ modules/32bit/code/32bit/main/src/apps/bm_app_reverb.c
@@ -16,15 +16,17 @@
 // 6. Try to add pre-delay
 // 7. Add two knob configuration to this, pre-delay & decay/room size
 
-#define MAX_DATA_SIZE 100
-float* data = NULL;
-int delay_index = 0;
-
-bm_ring_buffer_handler_t delay_buffer;
-bm_param delay_max_len;
-bm_param feedback;
-
 bool bypassed = false;
+
+typedef struct {
+    bm_ring_buffer_handler_t delay_buffer;
+    bm_param delay_length;
+    bm_param feedback;
+    float* data;
+    size_t max_delay_length;
+} comb_filter_t;
+
+comb_filter_t filter1;
 
 static void on_button_event(bool pressed) {
    bypassed = pressed;
@@ -40,14 +42,14 @@ static void on_midi_note_off(uint8_t channel, uint8_t control, uint8_t value) {
 
 static void on_midi_cc(uint8_t channel, uint8_t control, uint8_t value) {
     if (control == BM_MCC_BANK_A_CV1) {
-        float scaled = bm_utils_map_range((float)value, 0.0, 127.0, 10.0, MAX_DATA_SIZE - 1);
-        bm_param_set(&delay_max_len, scaled);
+        float scaled = bm_utils_map_range((float)value, 0.0, 127.0, 10.0, filter1.max_delay_length - 1);
+        bm_param_set(&filter1.delay_length, scaled);
         return;
     }
 
     if (control == BM_MCC_BANK_A_CV2) {
         float cc_norm = (float)value / 127.0;
-        bm_param_set(&feedback, cc_norm);
+        bm_param_set(&filter1.feedback, cc_norm);
         return;
     }
 }
@@ -66,27 +68,28 @@ inline static void process_audio(size_t n_samples, const int16_t* input, int16_t
 
         // 1. for the left channel
         float curr = bm_audio_norm(input[lc]);
-        float delayed = bm_ring_buffer_lookup(&delay_buffer, bm_param_get(&delay_max_len));
-        float next = curr + delayed * bm_param_get(&feedback);
+        float delayed = bm_ring_buffer_lookup(&filter1.delay_buffer, bm_param_get(&filter1.delay_length));
+        float next = curr + delayed * bm_param_get(&filter1.feedback);
 
-        bm_ring_buffer_add(&delay_buffer, next);
+        bm_ring_buffer_add(&filter1.delay_buffer, next);
         output[lc] = bm_audio_denorm(delayed);
     }
 }
 
 static void init(bm_app_host_t host) {
-    bm_param_init(&delay_max_len, MAX_DATA_SIZE, 0.01);
-    bm_param_init(&feedback, 0.7, 0.1);
-    data = calloc(MAX_DATA_SIZE, sizeof(float));
+    filter1.max_delay_length = 100;
+    bm_param_init(&filter1.delay_length, 10, 0.01);
+    bm_param_init(&filter1.feedback, 0.7, 0.1);
+    filter1.data = calloc(filter1.max_delay_length, sizeof(float));
     bm_ring_buffer_config_t buffer_config = {
-        .data = data,
-        .size = MAX_DATA_SIZE
+        .data = filter1.data,
+        .size = filter1.max_delay_length
     };
-    bm_init_ring_buffer(buffer_config, &delay_buffer);
+    bm_init_ring_buffer(buffer_config, &filter1.delay_buffer);
 }
 
 static void destroy() {
-    free(data);
+    free(filter1.data);
 }
 
 bm_app_t bm_load_app_reverb() {
