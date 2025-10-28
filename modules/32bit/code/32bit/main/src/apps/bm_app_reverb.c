03cbe326940b406236a680731aa081a2ac42cb66
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-10-28 16:07:43 +0530

    Implement the classic reverb.

--- modules/32bit/code/32bit/main/src/apps/bm_app_reverb.c
+++ modules/32bit/code/32bit/main/src/apps/bm_app_reverb.c
@@ -7,13 +7,14 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 
 // Tasks for Reverbs
 // [x] 1. Build the Comb Filter
 // [x] 2. Test with wheather we need configurable history or not
 // [x] 3. Build the all pass filter
 // [x] 4. Same as for the history
-// 5. Build a reverb out of it
+// [x] 5. Build a reverb out of it
 // 6. Try to add pre-delay
 // 7. Add two knob configuration to this, pre-delay & decay/room size
 
@@ -32,7 +33,7 @@ void comb_filter_init(comb_filter_t* filter, size_t max_delay_length, float feed
     filter->max_delay_length = max_delay_length;
     bm_param_init(&filter->delay_length, filter->max_delay_length, 0.01);
     bm_param_init(&filter->feedback, feedback, 0.1);
-    filter->data = calloc(filter->max_delay_length, sizeof(float));
+    filter->data = (float*)allocate_psram(filter->max_delay_length * sizeof(float));
     bm_ring_buffer_config_t buffer_config = {
         .data = filter->data,
         .size = filter->max_delay_length
@@ -41,7 +42,7 @@ void comb_filter_init(comb_filter_t* filter, size_t max_delay_length, float feed
 }
 
 void comb_filter_destroy(comb_filter_t* filter) {
-    free(filter->data);
+    free_psram(filter->data);
 }
 
 float comb_filter_process(comb_filter_t* filter, float input) {
@@ -67,29 +68,38 @@ void comb_filter_set_feedback_cc(comb_filter_t* filter, uint8_t value) {
 // *** START ALL PASS FILTER ***
 
 typedef struct {
-    float* delay_buffer;
+    float* data;
+    bm_ring_buffer_handler_t delay_buffer;
     size_t delay_length;
+    size_t max_delay_length;
     int delay_index;
     float feedback;
 } all_pass_filter_t;
 
-void all_pass_filter_init(all_pass_filter_t* filter, size_t delay_length, float feedback) {
+void all_pass_filter_init(all_pass_filter_t* filter, size_t max_delay_length, float feedback) {
     filter->delay_index = 0;
+    filter->max_delay_length = max_delay_length;
+    filter->delay_length = max_delay_length - 1;
+    filter->data = (float*)allocate_psram(filter->delay_length * sizeof(float));
+    bm_ring_buffer_config_t buffer_config = {
+        .data = filter->data,
+        .size = filter->max_delay_length
+    };
+    bm_init_ring_buffer(buffer_config, &filter->delay_buffer);
+
     filter->feedback = feedback;
-    filter->delay_length = delay_length;
-    filter->delay_buffer = calloc(filter->delay_length, sizeof(float));
 }
 
 void all_pass_filter_destroy(all_pass_filter_t* filter) {
-    free(filter->delay_buffer);
+    free_psram(filter->data);
 }
 
 float all_pass_filter_process(all_pass_filter_t* filter, float input) {
-    float delayed = filter->delay_buffer[filter->delay_index];
+    float delayed = bm_ring_buffer_lookup(&filter->delay_buffer , filter->delay_length);
     float processed = delayed + input * -filter->feedback;
+    float to_buffer = input + processed * filter->feedback;
 
-    filter->delay_buffer[filter->delay_index] = input + processed * filter->feedback;
-    filter->delay_index = (filter->delay_index + 1) % filter->delay_length;
+    bm_ring_buffer_add(&filter->delay_buffer, to_buffer);
 
     return processed;
 }
@@ -102,6 +112,19 @@ comb_filter_t comb3;
 comb_filter_t comb4;
 all_pass_filter_t allpass1;
 all_pass_filter_t allpass2;
+all_pass_filter_t allpass3;
+all_pass_filter_t allpass4;
+
+void set_rt60(float rt60) {
+    // g = 10^(-3*(d/Fs)/RT60)
+    float common = (-3.0 / 44100.0) / rt60;
+    comb_filter_t* filters[] = {&comb1, &comb2, &comb3, &comb4};
+    for (int lc=0; lc<4; lc++) {
+        comb_filter_t* filter = filters[lc];
+        float feedback = powf(10.0f, common * filter->delay_length.target_value);
+        bm_param_set(&filter->feedback, feedback);
+    }
+}
 
 static void on_button_event(bool pressed) {
    bypassed = pressed;
@@ -116,15 +139,19 @@ static void on_midi_note_off(uint8_t channel, uint8_t control, uint8_t value) {
 }
 
 static void on_midi_cc(uint8_t channel, uint8_t control, uint8_t value) {
-    // if (control == BM_MCC_BANK_A_CV1) {
-    //     comb_filter_set_delay_length_cc(&filter1, value);
-    //     return;
-    // }
+    if (control == BM_MCC_BANK_A_CV1) {
+        // This controls the room_size, rt60 is handles in seconds.
+        float rt60 = bm_utils_map_range(value, 0.0, 127.0, 0.1, 3.0);
+        set_rt60(rt60);
+        return;
+    }
 
-    // if (control == BM_MCC_BANK_A_CV2) {
-    //     comb_filter_set_feedback_cc(&filter1, value);
-    //     return;
-    // }
+    if (control == BM_MCC_BANK_A_CV2) {
+        // size_t delay_length = bm_utils_map_range(value, 0.0, 127.0, 50.0, 400.0);
+        // allpass1.delay_length = delay_length;
+        // allpass2.delay_length = delay_length + delay_length * 0.1;
+        return;
+    }
 }
 
 static void on_midi_bpm(uint16_t bpm) {
@@ -153,8 +180,10 @@ inline static void process_audio(size_t n_samples, const int16_t* input, int16_t
         float allpass_processed = 0.0f;
         allpass_processed = all_pass_filter_process(&allpass1, comb_processed);
         allpass_processed = all_pass_filter_process(&allpass2, allpass_processed);
+        allpass_processed = all_pass_filter_process(&allpass3, allpass_processed);
+        allpass_processed = all_pass_filter_process(&allpass4, allpass_processed);
 
-        float processed = comb_processed;
+        float processed = allpass_processed;
 
         output[lc] = bm_audio_denorm(processed);
     }
@@ -165,8 +194,10 @@ static void init(bm_app_host_t host) {
     comb_filter_init(&comb2, 1617, 0.7286);
     comb_filter_init(&comb3, 1491, 0.7468);
     comb_filter_init(&comb4, 1422, 0.7570);
-    all_pass_filter_init(&allpass1, 142, 0.70);
-    all_pass_filter_init(&allpass2, 56, 0.70);
+    all_pass_filter_init(&allpass1, 142, 0.80);
+    all_pass_filter_init(&allpass2, 56, 0.80);
+    all_pass_filter_init(&allpass3, 280, 0.80);
+    all_pass_filter_init(&allpass4, 400, 0.80);
 }
 
 static void destroy() {
@@ -176,6 +207,8 @@ static void destroy() {
     comb_filter_destroy(&comb4);
     all_pass_filter_destroy(&allpass1);
     all_pass_filter_destroy(&allpass2);
+    all_pass_filter_destroy(&allpass3);
+    all_pass_filter_destroy(&allpass4);
 }
 
 bm_app_t bm_load_app_reverb() {
