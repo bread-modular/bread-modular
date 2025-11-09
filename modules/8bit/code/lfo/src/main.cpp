78f1c1f5b0e8b766a74cab0df46c0f2081c0c29a
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-11-09 15:24:39 +0530

    Add a better curve for the CV1

--- modules/8bit/code/lfo/src/main.cpp
+++ modules/8bit/code/lfo/src/main.cpp
@@ -26,42 +26,6 @@ constexpr uint32_t LFO_MAX_FREQ_MILLIHZ = 20000; // 20 Hz
 constexpr uint32_t TCB_CLOCK_HZ = F_CPU / 2;
 constexpr uint16_t TCB_COMPARE_VALUE = TCB_CLOCK_HZ / LFO_SAMPLE_RATE_HZ;
 
-constexpr uint32_t LOG2_RATIO_Q16 = 500948; // log2(200) in Q16 format for log-style CV mapping
-const uint16_t POW2_FRAC_Q16[256] PROGMEM = {
-  65536,  65714,  65892,  66071,  66250,  66429,  66609,  66790,
-  66971,  67153,  67335,  67517,  67700,  67884,  68068,  68252,
-  68438,  68623,  68809,  68996,  69183,  69370,  69558,  69747,
-  69936,  70126,  70316,  70507,  70698,  70889,  71082,  71274,
-  71468,  71661,  71856,  72050,  72246,  72442,  72638,  72835,
-  73032,  73230,  73429,  73628,  73828,  74028,  74229,  74430,
-  74632,  74834,  75037,  75240,  75444,  75649,  75854,  76060,
-  76266,  76473,  76680,  76888,  77096,  77305,  77515,  77725,
-  77936,  78147,  78359,  78572,  78785,  78998,  79212,  79427,
-  79642,  79858,  80075,  80292,  80510,  80728,  80947,  81166,
-  81386,  81607,  81828,  82050,  82273,  82496,  82719,  82944,
-  83169,  83394,  83620,  83847,  84074,  84302,  84531,  84760,
-  84990,  85220,  85451,  85683,  85915,  86148,  86382,  86616,
-  86851,  87086,  87322,  87559,  87796,  88034,  88273,  88513,
-  88752,  88993,  89234,  89476,  89719,  89962,  90206,  90451,
-  90696,  90942,  91188,  91436,  91684,  91932,  92181,  92431,
-  92682,  92933,  93185,  93438,  93691,  93945,  94200,  94455,
-  94711,  94968,  95226,  95484,  95743,  96002,  96263,  96524,
-  96785,  97048,  97311,  97575,  97839,  98104,  98370,  98637,
-  98905,  99173,  99442,  99711,  99982, 100253, 100524, 100797,
- 101070, 101344, 101619, 101895, 102171, 102448, 102726, 103004,
- 103283, 103564, 103844, 104126, 104408, 104691, 104975, 105260,
- 105545, 105831, 106118, 106406, 106694, 106984, 107274, 107565,
- 107856, 108149, 108442, 108736, 109031, 109326, 109623, 109920,
- 110218, 110517, 110816, 111117, 111418, 111720, 112023, 112327,
- 112631, 112937, 113243, 113550, 113858, 114167, 114476, 114787,
- 115098, 115410, 115723, 116036, 116351, 116667, 116983, 117300,
- 117618, 117937, 118257, 118577, 118899, 119221, 119544, 119869,
- 120194, 120519, 120846, 121174, 121502, 121832, 122162, 122493,
- 122825, 123158, 123492, 123827, 124163, 124500, 124837, 125176,
- 125515, 125855, 126197, 126539, 126882, 127226, 127571, 127917,
- 128263, 128611, 128960, 129310, 129660, 130012, 130364, 130718
-};
-
 SimpleMIDI MIDI;
 // TODO: Change this to real serial port. (We need to update the schematic for that)
 SoftwareSerial logger(-1, LOGGER_PIN_TX);
@@ -74,17 +38,13 @@ volatile uint8_t waveformIndexA = WaveTables::Sine;
 volatile uint8_t waveformIndexB = WaveTables::Sine;
 volatile uint8_t waveformBlend = 0;
 
-uint32_t mapCvToFrequencyLog(uint16_t cvValue) {
-  uint32_t exponentQ16 = ((uint32_t)cvValue * LOG2_RATIO_Q16) / 1023UL;
-  uint8_t integerPart = exponentQ16 >> 16;
-  if (integerPart > 7) {
-    integerPart = 7;
-  }
-  uint16_t fracPart = exponentQ16 & 0xFFFF;
-  uint8_t tableIndex = fracPart >> 8;
-  uint16_t fracMultiplier = pgm_read_word(&POW2_FRAC_Q16[tableIndex]);
-  uint32_t ratioQ16 = (uint32_t)fracMultiplier << integerPart;
-  uint32_t freqMilliHz = (LFO_MIN_FREQ_MILLIHZ * ratioQ16) >> 16;
+uint32_t mapCvToFrequencyCurve(uint16_t cvValue) {
+  uint32_t range = LFO_MAX_FREQ_MILLIHZ - LFO_MIN_FREQ_MILLIHZ;
+  uint32_t normalized = ((uint32_t)cvValue << 16) / 1023UL; // Q16 0-1
+  uint32_t normalizedSq = ((uint64_t)normalized * normalized) >> 16; // Q16
+  uint32_t normalizedCube = ((uint64_t)normalizedSq * normalized) >> 16; // Q16
+  uint32_t curve = (normalized + normalizedSq + normalizedCube) / 3;
+  uint32_t freqMilliHz = LFO_MIN_FREQ_MILLIHZ + ((uint64_t)range * curve) >> 16;
   if (freqMilliHz > LFO_MAX_FREQ_MILLIHZ) {
     freqMilliHz = LFO_MAX_FREQ_MILLIHZ;
   }
@@ -210,7 +170,7 @@ void setup() {
   // Prime the phase increment and waveform so the LFO starts immediately
   uint16_t initialCv1 = analogRead(PIN_CV1);
   uint16_t initialCv2 = analogRead(PIN_CV2);
-  updateLfoPhaseIncrement(mapCvToFrequencyLog(initialCv1));
+  updateLfoPhaseIncrement(mapCvToFrequencyCurve(initialCv1));
   updateWaveformMix(initialCv2);
 
   // Setup MIDI
@@ -240,7 +200,7 @@ void loop() {
 
   // Update the LFO frequency based on CV1 every loop iteration
   uint16_t cv1 = analogRead(PIN_CV1);
-  uint32_t freqMilliHz = mapCvToFrequencyLog(cv1);
+  uint32_t freqMilliHz = mapCvToFrequencyCurve(cv1);
   updateLfoPhaseIncrement(freqMilliHz);
 
   uint16_t cv2 = analogRead(PIN_CV2);
