e484992f05978e8872394426d2422cd892d42d27
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-11-09 06:07:14 +0530

    Add support for two complex waveforms

--- modules/8bit/code/lfo/src/WaveTables.h
+++ modules/8bit/code/lfo/src/WaveTables.h
@@ -6,19 +6,23 @@
 
 namespace WaveTables {
 
-constexpr uint8_t kWaveformCount = 4;
+constexpr uint8_t kWaveformCount = 6;
 constexpr size_t kTableSize = 256;
 
 enum Waveform : uint8_t {
   Sine = 0,
   Triangle = 1,
   Saw = 2,
-  Random = 3,
+  Complex1 = 3,
+  Complex2 = 4,
+  Random = 5,
 };
 
 extern const uint8_t SINE_TABLE[kTableSize] PROGMEM;
 extern const uint8_t TRIANGLE_TABLE[kTableSize] PROGMEM;
 extern const uint8_t SAW_TABLE[kTableSize] PROGMEM;
+extern const uint8_t COMPLEX1_TABLE[kTableSize] PROGMEM;
+extern const uint8_t COMPLEX2_TABLE[kTableSize] PROGMEM;
 extern const uint8_t RANDOM_TABLE[kTableSize] PROGMEM;
 
 inline uint8_t readSample(Waveform waveform, uint8_t index) {
@@ -29,6 +33,10 @@ inline uint8_t readSample(Waveform waveform, uint8_t index) {
       return pgm_read_byte(&TRIANGLE_TABLE[index]);
     case Saw:
       return pgm_read_byte(&SAW_TABLE[index]);
+    case Complex1:
+      return pgm_read_byte(&COMPLEX1_TABLE[index]);
+    case Complex2:
+      return pgm_read_byte(&COMPLEX2_TABLE[index]);
     case Random:
     default:
       return pgm_read_byte(&RANDOM_TABLE[index]);
